06e412848b1a3cda30f4a023eb144f22cfa6ccfa
umrgaeshan <umegaeshan@gmail.com>
2024-10-24 13:49:52 +0530

    cal

--- main.c
+++ main.c
@@ -1,107 +1,86 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int main() {
+    float ans;
+    float history[100];  // Array to store history of results
+    int run;
 
-float ans;
-char history[100];
-char answer[100];
-int run;
-printf("Insert Run count = ");
+    printf("Insert Run count = ");
+    scanf("%d", &run);
 
-scanf("%d",&run);
+    for(int j = 0; j < run; j++) {
+        printf("\n");
+        printf("*****************************\n");
+        printf("\n-------  Operations--------\n");
+        printf("Addition \t->  1\n");
+        printf("Subtraction \t->  2 \n");
+        printf("Multiplication \t->  3\n");
+        printf("Division \t->  4\n");
+        printf("Power    \t -> 5\n");
+        printf("History   \t  -> 6 \n");
+        printf("\n******************************\n");
 
-for(int j=0;j<run;j++)
-{
+        printf("\n");
 
- printf("\n");
-    printf("*****************************\n");
-    printf("\n-------  Operations--------\n");
-    printf("Addition \t->  1\n");
-    printf("Subtraction \t->  2 \n");
-    printf("Multiplication \t->  3\n");
-    printf("Division \t->  4\n");
-    printf("Power    \t -> 5\n");
-    printf("History   \t  -> 6 \n");
-    printf("\n******************************\n");
+        float num1;
+        printf("Insert Number 1: ");
+        scanf("%f", &num1);
 
-    printf("\n");
-    float num1;
-    printf("Inset Number 1 : ",num1);
-    scanf("%f",&num1);
+        if (num1 == 6) {
+            printf("History: \n");
 
-if(num1==6)
-{
-        printf("History ");
+            for (int i = 0; i < j; i++) {
+                printf("%d: %.2f\n", i + 1, history[i]);
+            }
 
-            for(int i=0;i<run;i++)
-        {
-            printf("%d",history[i]);
+            continue;
         }
 
-        for(int i=0; i<run;i++)
-{
-    scanf("%d",&history[i]);
-}
+        float num2;
+        printf("Insert Number 2: ");
+        scanf("%f", &num2);
 
+        int operation;
+        printf("\nSelect your operation (Ex: 1): ");
+        scanf("%d", &operation);
 
+        switch(operation) {
+            case 1:
+                ans = num1 + num2;
+                break;
+            case 2:
+                ans = num1 - num2;
+                break;
+            case 3:
+                ans = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    ans = num1 / num2;
+                } else {
+                    printf("Error: Division by zero.\n");
+                    continue;
+                }
+                break;
+            case 5:
+                ans = pow(num1, num2);
+                break;
+            case 6:
+                printf("History: \n");
 
-}
+                for (int i = 0; i < j; i++) {
+                    printf("%d: %.2f\n", i + 1, history[i]);
+                }
+                continue;
+            default:
+                printf("Invalid operation.\n");
+                continue;
+        }
 
-else
-{
-
-
-
-    float num2;
-    printf("Insert number 2 :",num2);
-    scanf("%f",&num2);
-
- int operation;
-
-
-    printf("\n******************************\n");
-    printf("\n");
-    printf("Select your operations (Ex : 1) :",operation);
-    scanf("%d", &operation);
-
-
-
-
-    switch(operation)
-    {
-    case 1:
-              ans=num1+num2 ;
-
-              break;
-    case 2:
-             ans=num1-num2 ;
-
-             break;
-    case 3:
-             ans=num1*num2;
-
-             break;
-    case 4:
-              ans=num1/num2;
-
-              break;
-
-
-    case 5:
-              ans=num1*num2   ;
-              break;
-
-
-
-    default:
-               printf("Invalid Operator \t");
-               break;
+        printf("Answer is: %.2f\n\n", ans);
+        history[j] = ans;
     }
-printf("Answer is :  %.2f \n\n", ans);
 
-
-}
-}
     return 0;
 }
